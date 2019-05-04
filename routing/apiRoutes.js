157ae8a55efee65c4dca2157ff84da6a04614b9a
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-05-03 23:35:02 -0600

    console log data

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,8 +2,8 @@ var path = require( "path" );
 var express = require( "express" )
 
 module.exports = function ( app ) {
-    app.get( "/api/friends", function ( res, req ) {
-        res.json( friendData )
+    app.get( "/api/friends", function ( req, res ) {
+        res.json( friendsData )
     } )
     // app.post( "/app/data/friends.js", function ( res, req ) {
     //     if{}
